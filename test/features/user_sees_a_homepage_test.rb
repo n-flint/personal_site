8f916b59a92eaa9d1b2b9f4e2be2bdce462860f9
Noah Flint <noah.flint24@gmail.com>
2019-03-09 12:29:37 -0700

    created new test to see about page

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_can_not_see_pages_that_do_not_exist
+    visit '/fake'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
 end
